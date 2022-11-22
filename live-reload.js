7b80ea89bb8b497f3d7accfdefb504d102320873
ORESoftware <alex@oresoftware.com>
2022-11-21 21:24:48 -0600

    update live-reload script

--- live-reload.js
+++ live-reload.js
@@ -2,8 +2,9 @@
 
 const fs = require('fs');
 const ws = require('ws');
+const cp = require('child_process');
 
-const wss  ={
+const wss = {
     connections: new Set()
 }
 
@@ -18,16 +19,30 @@ server.on('listening', () => {
     console.log('server listening on interface:', serverConfig);
 });
 
+server.on('error', (err) => {
+    console.error(err);
+    console.log('Server probably already listening:');
+    console.log(String(cp.execSync('lsof -i :5900')));
+    process.exit(1);
+});
+
+
 let to = null;
 
-fs.watch('index.html', ev => {
+const onChange = ev => {
     clearTimeout(to);
     to = setTimeout(() => {
-        for(const c of wss.connections){
+        for (const c of wss.connections) {
             c.send(JSON.stringify({watcher: ev}));
         }
     }, 500);
-});
+}
+
+// evidently, no core api for watching multiple files in one call, lame
+fs.watch('index.html', onChange);
+fs.watch('main.js', onChange);
+fs.watch('style.css', onChange);
+
 
 server.on('connection', c => {
     wss.connections.add(c);

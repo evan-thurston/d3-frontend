a3b37813164177693cffa7bea387d26bec8910b0
Evan <evanthurston@gmail.com>
2022-12-03 22:24:00 -0600

    Mobile fix & histogram smoothing attempt

--- src/lib/Histogram.svelte
+++ src/lib/Histogram.svelte
@@ -14,6 +14,21 @@
 		.scaleLinear()
 		.domain([0, Math.max(...data)])
 		.range([30, height]);
+	
+	// TODO: algorithm to smooth out histogram
+	// const numBefore = (num) => {
+	// 	if(data.indexOf(num) === 0) return
+	// 	else return data[data.indexOf(num) - 1]
+	// }
+
+	// const numAfter = (num) => {
+	// 	if(data.indexOf(num) === data.length-2) return
+	// 	else return data[data.indexOf(num) +1]
+	// }
+
+	// const avg = (num) => {
+	// 	return Math.round((numBefore(num) + num + numAfter(num)) / 3)
+	// }
 </script>
 
 <div class="chart">
@@ -21,7 +36,7 @@
 		<div
 			class="col"
 			style="height: {yScale(num)}px; width: {width / data.length -
-				2}px; background-color: {color}"
+				3}px; background-color: {color}"
 		>
 			<p>
 				{num}
@@ -35,10 +50,10 @@
 		@apply mb-2;
 	}
 	.chart .col {
-		@apply p-1 pt-0 mx-[1px] align-bottom inline-block
+		@apply sm:p-1 pt-0 mx-[1px] align-bottom inline-block
             font-sans text-center text-slate-600;
 	}
 	.chart .col p {
-		@apply text-[0.5rem] lg:text-xs 2xl:text-sm;
+		@apply text-[0.3rem] sm:text-[0.4rem] md:text-[0.6rem] lg:text-xs 2xl:text-sm;
 	}
 </style>

ddc91f75c26ae0eb8f8cd4528c85b351664180c6
Evan <evanthurston@gmail.com>
2023-04-11 20:31:38 -0500

    mobile test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -89,10 +89,10 @@
     };
 
     const addNode = () => {
-        let nodeId = 'new node '
+        let nodeId = 'new node ';
         let count = 1;
 
-        while(nodes.find(({id}) => id === nodeId + count)) {
+        while (nodes.find(({ id }) => id === nodeId + count)) {
             count++;
         }
 
@@ -157,22 +157,22 @@
     bind:innerWidth={width}
 />
 
-{#if !loaded}
+<!-- {#if !loaded}
     <h1 class="loadingText">loading...</h1>
 {:else if !simulationSelected}
     <PresetSelector {sendPresetData} />
-{:else}
-    <DataDrawer
-        {nodes}
-        {selectedNodes}
-        {selectNode}
-        bind:updatesPaused
-        {updateList}
-        {addNode}
-        {deleteNode}
-    />
+{:else} -->
+<DataDrawer
+    {nodes}
+    {selectedNodes}
+    {selectNode}
+    bind:updatesPaused
+    {updateList}
+    {addNode}
+    {deleteNode}
+/>
 
-    {#key restartSim}
+<!-- {#key restartSim}
         <MainGraph
             {width}
             {height}
@@ -188,9 +188,9 @@
             {grid}
             bind:restartSim
         />
-    {/key}
+    {/key} -->
 
-    <ControlPanel
+<!-- <ControlPanel
         bind:physicsPaused
         bind:emitterIndex
         {emitters}
@@ -202,7 +202,7 @@
         bind:grid
         {restart}
     />
-{/if}
+{/if} -->
 
 <style lang="postcss">
     h1.loadingText {
